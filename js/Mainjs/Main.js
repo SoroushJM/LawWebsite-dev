e2ed90b4f444a5bf6640311e8d1cdde2f6842ab7
SoroushJm <soroush.jamili@gmail.com>
2024-10-29 14:23:51 +0330

    added language drop down and some miner chabges

--- js/Mainjs/Main.js
+++ js/Mainjs/Main.js
@@ -1,4 +1,4 @@
-
+//set up carousel
 console.log("main js loaded");
 const myCarouselElement = document.querySelector('#carouselMain');
 
@@ -6,3 +6,36 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 10000,
   touch: true
 })
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const languageSelect = document.getElementById("language-select");
+
+  // Function to switch language based on selected value
+  function switchLanguage(language) {
+      const elements = document.querySelectorAll(".translatable");
+      elements.forEach((element) => {
+          const translation = element.getAttribute(`data-${language}`);
+          if (translation) {
+              element.textContent = translation;
+          }
+      });
+
+      // For Arabic or Farsi, set RTL direction
+      if (language === 'ar' || language === 'fa') {
+          document.body.setAttribute("dir", "rtl");
+      } else {
+          document.body.removeAttribute("dir");
+      }
+  }
+
+  // Event listener to change language on selection
+  languageSelect.addEventListener("change", (event) => {
+      const selectedLanguage = event.target.value;
+      switchLanguage(selectedLanguage);
+  });
+
+  // Initial language load
+  switchLanguage(languageSelect.value);
+});
